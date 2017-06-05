e28bf481a35ded8e51ba12557ec9b884cb53f14c
Bruno <blgoes@gmail.com>
2017-06-05 19:52:45 -0300

    adicionando thymeleaf

--- src/main/java/br/com/gerenciador/controller/IngredienteController.java
+++ src/main/java/br/com/gerenciador/controller/IngredienteController.java
@@ -4,14 +4,16 @@ package br.com.gerenciador.controller;
 import br.com.gerenciador.api.Ingrediente;
 import br.com.gerenciador.service.IngredienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-@RestController
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
 public class IngredienteController {
 
     private IngredienteService ingredienteService;
@@ -21,12 +23,18 @@ public class IngredienteController {
     }
 
     @RequestMapping("/ingredientes/{id}")
-    public Ingrediente get(@PathVariable("id") Long id) {
+    public @ResponseBody Ingrediente get(@PathVariable("id") Long id) {
         return ingredienteService.getById(id);
     }
 
     @RequestMapping("/ingredientes")
-    public ArrayList<Ingrediente> getAll() {
-        return ingredienteService.getAll();
+    public @ResponseBody ArrayList<Ingrediente> getAll() {
+           return ingredienteService.getAll();
+
+    }
+    @RequestMapping(value = "/greeting" , method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
